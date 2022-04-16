c8c62a81d61dc8d627323ff4a6b8aec9bffdacf5
falkachu <falkachu@posteo.de>
2022-04-16 13:21:37 +0200

    feat: more colors

--- strcolor/strcolor.go
+++ strcolor/strcolor.go
@@ -2,18 +2,86 @@ package strcolor
 
 import "fmt"
 
+const (
+	RESET         = "\u001b[0m"
+	BLACK         = "\u001b[30m"
+	RED           = "\u001b[31m"
+	GREEN         = "\u001b[32m"
+	YELLOW        = "\u001b[33m"
+	BLUE          = "\u001b[34m"
+	MAGENTA       = "\u001b[35m"
+	CYAN          = "\u001b[36m"
+	WHITE         = "\u001b[37m"
+	BLACKBRIGHT   = "\u001b[90m"
+	REDBRIGHT     = "\u001b[91m"
+	GREENBRIGHT   = "\u001b[92m"
+	YELLOWBRIGHT  = "\u001b[93m"
+	BLUEBRIGHT    = "\u001b[94m"
+	MAGENTABRIGHT = "\u001b[95m"
+	CYANBRIGHT    = "\u001b[96m"
+	WHITEBRIGHT   = "\u001b[97m"
+)
+
+func Black(str string) string {
+	return fmt.Sprintf("%s%s%s", BLACK, str, RESET)
+}
+
 func Red(str string) string {
-	return fmt.Sprintf("\u001b[31m%s\u001b[0m", str)
+	return fmt.Sprintf("%s%s%s", RED, str, RESET)
 }
 
-func GreenBright(str string) string {
-	return fmt.Sprintf("\u001b[92m%s\u001b[0m", str)
+func Green(str string) string {
+	return fmt.Sprintf("%s%s%s", GREEN, str, RESET)
 }
 
-func YellowBright(str string) string {
-	return fmt.Sprintf("\u001b[93m%s\u001b[0m", str)
+func Yellow(str string) string {
+	return fmt.Sprintf("%s%s%s", YELLOW, str, RESET)
 }
 
 func Blue(str string) string {
-	return fmt.Sprintf("\u001b[34m%s\u001b[0m", str)
+	return fmt.Sprintf("%s%s%s", BLUE, str, RESET)
+}
+
+func Magenta(str string) string {
+	return fmt.Sprintf("%s%s%s", MAGENTA, str, RESET)
+}
+
+func Cyan(str string) string {
+	return fmt.Sprintf("%s%s%s", CYAN, str, RESET)
+}
+
+func White(str string) string {
+	return fmt.Sprintf("%s%s%s", WHITE, str, RESET)
+}
+
+func BlackBright(str string) string {
+	return fmt.Sprintf("%s%s%s", BLACKBRIGHT, str, RESET)
+}
+
+func RedBright(str string) string {
+	return fmt.Sprintf("%s%s%s", REDBRIGHT, str, RESET)
+}
+
+func GreenBright(str string) string {
+	return fmt.Sprintf("%s%s%s", GREENBRIGHT, str, RESET)
+}
+
+func YellowBright(str string) string {
+	return fmt.Sprintf("%s%s%s", YELLOWBRIGHT, str, RESET)
+}
+
+func BlueBright(str string) string {
+	return fmt.Sprintf("%s%s%s", BLUEBRIGHT, str, RESET)
+}
+
+func MagentaBright(str string) string {
+	return fmt.Sprintf("%s%s%s", MAGENTABRIGHT, str, RESET)
+}
+
+func CyanBright(str string) string {
+	return fmt.Sprintf("%s%s%s", CYANBRIGHT, str, RESET)
+}
+
+func WhiteBright(str string) string {
+	return fmt.Sprintf("%s%s%s", WHITEBRIGHT, str, RESET)
 }
--- strcolor/strcolor_test.go
+++ strcolor/strcolor_test.go
@@ -2,7 +2,7 @@ package strcolor
 
 import "testing"
 
-func TestBlue(t *testing.T) {
+func TestBlack(t *testing.T) {
 	type args struct {
 		str string
 	}
@@ -11,32 +11,12 @@ func TestBlue(t *testing.T) {
 		args args
 		want string
 	}{
-		{"Print Blue", args{"Blue"}, "\u001b[34mBlue\u001b[0m"},
+		{"Print Black", args{"Black"}, "\u001b[30mBlack\u001b[0m"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Blue(tt.args.str); got != tt.want {
-				t.Errorf("Blue() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestGreenBright(t *testing.T) {
-	type args struct {
-		str string
-	}
-	tests := []struct {
-		name string
-		args args
-		want string
-	}{
-		{"Print GreenBright", args{"GreenBright"}, "\u001b[92mGreenBright\u001b[0m"},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := GreenBright(tt.args.str); got != tt.want {
-				t.Errorf("GreenBright() = %v, want %v", got, tt.want)
+			if got := Black(tt.args.str); got != tt.want {
+				t.Errorf("Black() = %v, want %v", got, tt.want)
 			}
 		})
 	}
@@ -62,6 +42,186 @@ func TestRed(t *testing.T) {
 	}
 }
 
+func TestGreen(t *testing.T) {
+	type args struct {
+		str string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"Print Green", args{"Green"}, "\u001b[32mGreen\u001b[0m"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Green(tt.args.str); got != tt.want {
+				t.Errorf("Green() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestYellow(t *testing.T) {
+	type args struct {
+		str string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"Print Yellow", args{"Yellow"}, "\u001b[33mYellow\u001b[0m"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Yellow(tt.args.str); got != tt.want {
+				t.Errorf("Yellow() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestBlue(t *testing.T) {
+	type args struct {
+		str string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"Print Blue", args{"Blue"}, "\u001b[34mBlue\u001b[0m"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Blue(tt.args.str); got != tt.want {
+				t.Errorf("Blue() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMagenta(t *testing.T) {
+	type args struct {
+		str string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"Print Magenta", args{"Magenta"}, "\u001b[35mMagenta\u001b[0m"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Magenta(tt.args.str); got != tt.want {
+				t.Errorf("Magenta() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCyan(t *testing.T) {
+	type args struct {
+		str string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"Print Cyan", args{"Cyan"}, "\u001b[36mCyan\u001b[0m"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Cyan(tt.args.str); got != tt.want {
+				t.Errorf("Cyan() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestWhite(t *testing.T) {
+	type args struct {
+		str string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"Print White", args{"White"}, "\u001b[37mWhite\u001b[0m"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := White(tt.args.str); got != tt.want {
+				t.Errorf("White() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestBlackBright(t *testing.T) {
+	type args struct {
+		str string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"Print BlackBright", args{"BlackBright"}, "\u001b[90mBlackBright\u001b[0m"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := BlackBright(tt.args.str); got != tt.want {
+				t.Errorf("BlackBright() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestRedBright(t *testing.T) {
+	type args struct {
+		str string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"Print RedBright", args{"RedBright"}, "\u001b[91mRedBright\u001b[0m"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := RedBright(tt.args.str); got != tt.want {
+				t.Errorf("RedBright() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGreenBright(t *testing.T) {
+	type args struct {
+		str string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"Print GreenBright", args{"GreenBright"}, "\u001b[92mGreenBright\u001b[0m"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := GreenBright(tt.args.str); got != tt.want {
+				t.Errorf("GreenBright() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
 func TestYellowBright(t *testing.T) {
 	type args struct {
 		str string
@@ -76,7 +236,87 @@ func TestYellowBright(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := YellowBright(tt.args.str); got != tt.want {
-				t.Errorf("YelloBright() = %v, want %v", got, tt.want)
+				t.Errorf("YellowBright() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestBlueBright(t *testing.T) {
+	type args struct {
+		str string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"Print BlueBright", args{"BlueBright"}, "\u001b[94mBlueBright\u001b[0m"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := BlueBright(tt.args.str); got != tt.want {
+				t.Errorf("BlueBright() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMagentaBright(t *testing.T) {
+	type args struct {
+		str string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"Print MagentaBright", args{"MagentaBright"}, "\u001b[95mMagentaBright\u001b[0m"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := MagentaBright(tt.args.str); got != tt.want {
+				t.Errorf("MagentaBright() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCyanBright(t *testing.T) {
+	type args struct {
+		str string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"Print CyanBright", args{"CyanBright"}, "\u001b[96mCyanBright\u001b[0m"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := CyanBright(tt.args.str); got != tt.want {
+				t.Errorf("CyanBright() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestWhiteBright(t *testing.T) {
+	type args struct {
+		str string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"Print WhiteBright", args{"WhiteBright"}, "\u001b[97mWhiteBright\u001b[0m"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := WhiteBright(tt.args.str); got != tt.want {
+				t.Errorf("WhiteBright() = %v, want %v", got, tt.want)
 			}
 		})
 	}
